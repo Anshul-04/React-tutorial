c998054d3a5769353d16bb71e0ed0cdc6ae014c1
Anshul-04 <anshulonmars@gmail.com>
2025-04-27 17:17:32 +0530

    Outputting Conditional Expenses Content

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -12,22 +12,31 @@ export default function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
+  //filteredexpenses is an array of objects that match the selected year
+  const filteredexpenses = props.items.filter( (expense)=>{
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
+
+  let expensesContent = <p>No expenses found.</p>;
+  // If there are expenses that match the selected year, map them to ExpenseItem components
+  if(filteredexpenses.length > 0){
+    expensesContent = filteredexpenses.map((expense)=>(
+      <ExpenseItem 
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
       <ExpenseFilter 
         selected={filteredYear} 
         onChangeFilter={filterChangeHandler}
       />
-
-      {props.items.map((expense)=>(
-        <ExpenseItem 
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-      
+      {expensesContent}           
     </Card>
   );
 }
